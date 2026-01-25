23c9204beceda604d1b9679478f6db71a9f6dfdf
Alessiaa01 <occhionero.alessia01@gmail.com>
2026-01-25 09:54:02 +0100

    a

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,5 @@
--- Inserisci l'utente. Il DB assegnerà automaticamente id=1
-insert into users(name, surname, email) values('alessia', 'occhionero', 'alessia@gmail.com');
+-- Inserisci l'utente con la data di nascita (formato 'AAAA-MM-GG')
+INSERT INTO users (id,name, surname, email, date_of_birth) VALUES (1,'alessia', 'occhionero', 'alessia@gmail.com', '2001-03-27');
 
--- Inserisci le credenziali. Il DB assegnerà automaticamente id=1
--- Noi impostiamo manualmente user_id=1 per collegarlo all'utente 'paolo' appena creato
-insert into credentials(password, role, username, user_id,enabled) values('$2a$12$jxOF7FG6Hdjkv6OzdDNlnOedw4igAdVIVXqTO.r9wcP1pgyxdaiXa', 'ADMIN', 'alessia', 1,true);
+-- Inserisci le credenziali (collegando user_id=1)
+INSERT INTO credentials (password, role, username, user_id, enabled) VALUES ('$2a$12$AMC0tDbfcTEOKQd3BizQ3e1Ez5JLajLAIskHZt...4PORrJ8mRHd2', 'ADMIN', 'alessia', 1, true);
