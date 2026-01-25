4ddcbbcee99553f52fcc611f2cd07c581a391be7
Alessiaa01 <occhionero.alessia01@gmail.com>
2026-01-25 18:35:41 +0100

    agg

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,5 @@ INSERT INTO users (id,name, surname, email, date_of_birth) VALUES (1,'alessia', 
 
 -- Inserisci le credenziali (collegando user_id=1)
 INSERT INTO credentials (password, role, username, user_id, enabled) VALUES ('$2a$12$AMC0tDbfcTEOKQd3BizQ3e1Ez5JLajLAIskHZt...4PORrJ8mRHd2', 'ADMIN', 'alessia', 1, true);
+
+ALTER SEQUENCE users_seq RESTART WITH 100;
